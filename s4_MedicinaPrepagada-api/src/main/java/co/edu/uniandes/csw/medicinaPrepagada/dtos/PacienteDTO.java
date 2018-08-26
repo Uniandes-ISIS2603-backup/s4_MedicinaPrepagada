2d18f9168451ddbce452fe42dfdb5289524ad763
estudiante <estudiante@ISIS2603-323.sis.virtual.uniandes.edu.co>
2018-08-26 16:33:53 -0500

    DTOS de Paciente, Usuario y TarjetaCredito

---
+++ s4_MedicinaPrepagada-api/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/dtos/PacienteDTO.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.medicinaPrepagada.dtos;
+
+import java.io.Serializable;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ *
+ * PacienteDTO Objeto de transferencia de datos de Pacientes. Los DTO
+ * contienen las representaciones de los JSON que se transfieren entre el
+ * cliente y el servidor.
+ *
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "cedula":number,
+ *      "nombre":string,
+ *      "fechaNacimiento":string,
+ *      "direccion":string,
+ *      "numeroContacto":number,
+ *      "mail":string,
+ *      "eps": string}
+ *   }
+ * </pre> Por ejemplo un paciente se representa asi:<br>
+ *
+ * <pre>
+ *
+ *   {
+ *      "cedula":1029849123,
+ *      "nombre":"Pepito Perez",
+ *      "fechaNacimiento":"19/02/1982",
+ *      "direccion":"Av. 19 # 97 - 49",
+ *      "numeroContacto":1234567890,
+ *      "mail":"pepito@mail.com",
+ *      "eps": "la menos mala"}
+ *   }
+ *
+ * </pre>
+ *
+ * @author MIGUELHOYOS
+ */
+public class PacienteDTO implements Serializable{
+    
+    private Long cedula;
+    private String nombre;
+    private String fechaNacimiento;
+    private String direccion;
+    private Long numeroContacto;
+    private String mail;
+    private String eps;
+    
+    /**
+     * constructor por defecto
+     */
+    public PacienteDTO(){
+        
+    }
+    
+    /**
+     *  Conviertir Entity a DTO (Crea un nuevo DTO con los valores que recibe en
+     * la entidad que viene de argumento.
+     * 
+     * @param pacienteEntity: es la entidad para convertir en DTO
+     */
+    public PacienteDTO(PacienteEntity pacienteEntity){
+        if(pacienteEntity != null){
+            this.cedula = pacienteEntity.getCedula();
+            this.nombre = pacienteEntity.getNombre();
+            this.fechaNacimiento = pacienteEntity.getFechaNacimiento();
+            this.direccion = pacienteEntity.getDireccion();
+            this.numeroContacto = pacienteEntity.getNumeroContacto();
+            this.mail = pacienteEntity.getMail();
+            this.eps = pacienteEntity.getEps();
+        }
+    }
+
+    /**
+     * @return the cedula
+     */
+    public Long getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(Long cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the fechaNacimiento
+     */
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    /**
+     * @param fechaNacimiento the fechaNacimiento to set
+     */
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the numeroContacto
+     */
+    public Long getNumeroContacto() {
+        return numeroContacto;
+    }
+
+    /**
+     * @param numeroContacto the numeroContacto to set
+     */
+    public void setNumeroContacto(Long numeroContacto) {
+        this.numeroContacto = numeroContacto;
+    }
+
+    /**
+     * @return the mail
+     */
+    public String getMail() {
+        return mail;
+    }
+
+    /**
+     * @param mail the mail to set
+     */
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    /**
+     * @return the eps
+     */
+    public String getEps() {
+        return eps;
+    }
+
+    /**
+     * @param eps the eps to set
+     */
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
+    
+    /**
+     * convierte DTO a entity
+     * @return un entity con los valores del dto
+     */
+    public PacienteEntity toEntity(){
+        PacienteEntity pacienteEntity = new PacienteEntity();
+        pacienteEntity.setCedula(this.cedula);
+        pacienteEntity.setNombre(this.nombre);
+        pacienteEntity.setFechaNacimiento(this.fechaNacimiento);
+        pacienteEntity.setMail(this.mail);
+        pacienteEntity.setDireccion(this.direccion);
+        pacienteEntity.setNumeroContacto(this.numeroContacto);
+        pacienteEntity.setEps(this.eps);
+        return pacienteEntity;
+    }
+    
+    @Override
+    public String toString(){
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
---
+++ s4_MedicinaPrepagada-api/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/dtos/UsuarioDTO.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.medicinaPrepagada.dtos;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ ** UsuarioDTO Objeto de transferencia de datos de Usuarios. Los DTO
+ * contienen las representaciones de los JSON que se transfieren entre el
+ * cliente y el servidor.
+ *
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "login":string,
+ *      "contrasena":string,
+ *      "tipoUsuario":string
+ *   }
+ * </pre> Por ejemplo una editorial se representa asi:<br>
+ *
+ * <pre>
+ *
+ *   {
+ *      "login":"pepito420",
+ *      "contrasena":"345",
+ *      "tipoUsuario":"doctor"
+ *   }
+ *
+ * </pre>
+ * 
+ * @author MIGUELHOYOS
+ */
+public class UsuarioDTO {
+    
+    private String login;
+    private String contrasena;
+    private String tipoUsuario;
+    
+    /**
+     * Constructor por defecto
+     */
+    public UsuarioDTO(){
+        
+    }
+    
+     /**
+     * Conviertir Entity a DTO (Crea un nuevo DTO con los valores que recibe en
+     * la entidad que viene de argumento.
+     *
+     * @param usuarioEntity: Es la entidad que se va a convertir a DTO
+     */
+    public UsuarioDTO(UsuarioEntity usuarioEntity){
+        if(usuarioEntity != null){
+            this.login = usuarioEntity.getLogin();
+            this.contrasena = usuarioEntity.getContrasena();
+            this.tipoUsuario = usuarioEntity.getTipoUsuario();
+        }
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the contrasena
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * @param contrasena the contrasena to set
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    /**
+     * @return the tipoUsuario
+     */
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    /**
+     * @param tipoUsuario the tipoUsuario to set
+     */
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+    
+    /**
+     * Convertir DTO a Entity
+     *
+     * @return Un Entity con los valores del DTO
+     */
+    public UsuarioEntity toEntity(){
+        UsuarioEntity entity = new UsuarioEntity();
+        entity.setLogin(this.login);
+        entity.setContrasena(this.contrasena);
+        entity.setTipoUsuario(this.tipoUsuario);
+        return entity;
+    }
+    
+    @Override
+    public String toString(){
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+    
+}
