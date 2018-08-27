6082d6d88ae03d4148bedc77b7a50154fd96e44b
estudiante <estudiante@ISIS2603-319.sis.virtual.uniandes.edu.co>
2018-08-26 23:56:08 -0500

    Se agregan HorarioAtencion DTO, consultorio Resource, HorarioAtencionResource y SedeResource vacios

---
+++ s4_MedicinaPrepagada-api/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/resources/SedeResource.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.medicinaPrepagada.resources;
+
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author Simon Guzman
+ */
+
+@Path("sedes")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class SedeResource 
+{
+    
+    
+       private static final Logger LOGGER = Logger.getLogger(SedeResource.class.getName());
+    
+}
