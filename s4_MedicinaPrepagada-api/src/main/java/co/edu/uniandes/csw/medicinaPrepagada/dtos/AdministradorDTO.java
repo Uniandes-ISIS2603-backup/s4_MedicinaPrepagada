2e6f1385c110a6397818452736cbddfecca89631
estudiante <estudiante@ISIS2603-322.sis.virtual.uniandes.edu.co>
2018-08-26 19:10:09 -0500

    AdministradorDTO

--- s4_MedicinaPrepagada-api/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/dtos/AdministradorDTO.java
+++ s4_MedicinaPrepagada-api/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/dtos/AdministradorDTO.java
@@ -5,10 +5,55 @@
  */
 package co.edu.uniandes.csw.medicinaPrepagada.dtos;
 
+import java.io.Serializable;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 /**
  *
  * @author ni.ramirez10
  */
-public class AdministradorDTO {
+public class AdministradorDTO implements Serializable 
+{
+    /**
+     * Constructor vacio
+     */
     
+    public AdministradorDTO()
+    {
+        //Constrcutor vacio
+    }
+    
+    /**
+     * Crea un objeto AdministradorDTO a partir de un objeto AdministradorEntity.
+     * @param administradorEntity Entidad AdministradorEntity desde la cual se va a crear el
+     * nuevo objeto.
+     */
+    
+    /**public AdministradorDTO(AdministradorEntity administradorEntity) 
+    {
+        if (ordenMedicaEntity != null) 
+        {
+            
+        }
+    }
+     
+    /**
+    * Convierte un objeto AdministradorDTO a AdministradorEntity.
+    * @return Nueva objeto AdministradorEntity.
+    */
+    
+    /**public AdministradorEntity toEntity() 
+    {
+        AdministradorEntity administradorEntity = new AdministradorEntity();
+        return administradorEntity;
+    }
+    * */
+    
+    
+    @Override
+    public String toString() 
+    {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
 }
