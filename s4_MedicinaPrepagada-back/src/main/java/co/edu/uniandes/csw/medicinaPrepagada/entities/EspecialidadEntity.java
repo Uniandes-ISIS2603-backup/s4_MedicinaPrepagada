6ec7f763170176706ffc833a8c15b50d71888556
estudiante <estudiante@ISIS2603-341.sis.virtual.uniandes.edu.co>
2018-09-01 08:50:44 -0500

    Se agregan mis entities

---
+++ s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/entities/EspecialidadEntity.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.medicinaPrepagada.entities;
+
+/**
+ *
+ * @author Daniel Ivan Romero
+ */
+public class EspecialidadEntity {
+    
+    private long id;
+    private String nombre;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
