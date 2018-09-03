d9e7662bb05f357f109d9d2f6e13a0e6a956ecd2
estudiante <estudiante@ISIS2603-322.sis.virtual.uniandes.edu.co>
2018-09-03 08:56:12 -0500

    Correcion OrdenMedicEntity

--- s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/entities/OrdenMedicaEntity.java
+++ s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/entities/OrdenMedicaEntity.java
@@ -12,6 +12,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import uk.co.jemos.podam.common.PodamExclude;
@@ -31,6 +32,9 @@ public class OrdenMedicaEntity extends BaseEntity implements Serializable
     private String comentarios; 
     @Temporal(TemporalType.DATE)
     private Date validaHasta; 
+    
+    @OneToMany
+    private HistoriaClinicaEntity historias; 
     
     //@PodamExclude
     //@ManyToOne
