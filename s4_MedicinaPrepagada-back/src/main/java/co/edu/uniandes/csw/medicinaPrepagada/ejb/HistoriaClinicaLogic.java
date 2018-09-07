11f08ede1c37acfb57610b4518dcb4bc5402ecab
estudiante <estudiante@ISIS2603-322.sis.virtual.uniandes.edu.co>
2018-09-07 12:43:27 -0500

    Historia Clinica Logic

--- s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/ejb/HistoriaClinicaLogic.java
+++ s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/ejb/HistoriaClinicaLogic.java
@@ -5,10 +5,118 @@
  */
 package co.edu.uniandes.csw.medicinaPrepagada.ejb;
 
+import co.edu.uniandes.csw.medicinaPrepagada.entities.AdministradorEntity;
+import co.edu.uniandes.csw.medicinaPrepagada.entities.HistoriaClinicaEntity;
+import co.edu.uniandes.csw.medicinaPrepagada.entities.OrdenMedicaEntity;
+import co.edu.uniandes.csw.medicinaPrepagada.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.medicinaPrepagada.persistence.HistoriaClinicaPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
  * @author ni.ramirez10
  */
-public class HistoriaClinicaLogic {
+
+@Stateless
+public class HistoriaClinicaLogic 
+{
+     private static final Logger LOGGER = Logger.getLogger(HistoriaClinicaLogic.class.getName());
+
+    @Inject
+    private HistoriaClinicaPersistence persistence;
+    
+    /**
+     * Guardar una nueva historia clinica
+     * @param historiaEntity
+     * @return La entidad luego de persistirla
+     * @throws BusinessLogicException X
+     */
+    
+    public HistoriaClinicaEntity createHistoriaClinica(HistoriaClinicaEntity historiaEntity) throws BusinessLogicException 
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de creación de la historia clinica");
+
+        if (persistence.find(historiaEntity.getId()) != null ) 
+        {
+            throw new BusinessLogicException("La historia clinica ya existe");
+        }
+
+        persistence.create(historiaEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de creación de la historia clinica");
+        return historiaEntity;
+    }
+    
+     /**
+     * Obtener todas las historia clinicas existentes en la base de datos.
+     * @return una lista de historias clinicas.
+     */
+    
+    public List<HistoriaClinicaEntity> getHistoriasClinicas( )
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todas las historias clinicas");
+        List<HistoriaClinicaEntity> historias = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina proceso de consultar todas las historias clinicas");
+        return historias;
+    }
+    
+    /**
+     * Obtener una historia clinica por medio de su id.
+     * @param historiaId
+     * @return la historia clinica solicitada por medio de su id.
+     */
+    
+    public HistoriaClinicaEntity getHistoriaClinica(Long historiaId)
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar la historia clinica con id = {0}", historiaId);
+        HistoriaClinicaEntity historiaEntity = persistence.find(historiaId);
+        
+        if (historiaEntity == null) 
+        {
+            LOGGER.log(Level.SEVERE, "La historia clinica con el id = {0} no existe", historiaId);
+        }
+        
+        LOGGER.log(Level.INFO, "Termina proceso de consultar la historia clinica con id = {0}", historiaId);
+        return historiaEntity;
+    }
+    
+    /**
+     * Actualizar una historia clinica.
+     * @param historiaId
+     * @param historiaEntity
+     * @return la historia clinica con los cambios actualizados en la base de datos.
+     */
+    
+    public HistoriaClinicaEntity updateHistoriaClinica(Long historiaId, HistoriaClinicaEntity historiaEntity) 
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar la historia clinica con id = {0}", historiaId);
+        HistoriaClinicaEntity newEntity = persistence.update(historiaEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar la historia clinica con id = {0}", historiaEntity.getId());
+        return newEntity;
+    }
+    
+    /**
+     * Borrar una historia clinica
+     * @param historiaId
+     * @throws BusinessLogicException Si la historia a eliminar tiene ordenes medicas. 
+     */
+    
+    public void deleteHistoriaClinica(Long historiaId) throws BusinessLogicException 
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar la historia clinica con id = {0}", historiaId);
+        
+        List<OrdenMedicaEntity> ordenes = getHistoriaClinica(historiaId).getOrdenesMedicas(); 
+        
+        if (ordenes != null && !ordenes.isEmpty()) 
+        {
+            throw new BusinessLogicException("No se puede borrar la historia clinica con id = " + historiaId + " porque tiene ordenes"
+                    + " medicas asociadas");
+        }
+        persistence.delete(historiaId);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar la historia clinica con id = {0}", historiaId);
+    }
     
 }
