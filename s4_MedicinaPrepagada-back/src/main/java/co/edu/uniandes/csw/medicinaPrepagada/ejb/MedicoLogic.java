07b4f3dd430679d0abee2616e01b153ce3d8bda2
estudiante <estudiante@ISIS2603-341.sis.virtual.uniandes.edu.co>
2018-09-08 09:31:44 -0500

    Paso a la carpeta ejb de las clases de logica

--- s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/ejb/MedicoLogic.java
+++ s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/ejb/MedicoLogic.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.edu.uniandes.csw.medicinaPrepagada.logic;
+package co.edu.uniandes.csw.medicinaPrepagada.ejb;
 
 /**
  *
  * @author Daniel Ivan Romero
  */
-public class EspecialidadLogic {
+public class MedicoLogic {
     
 }
