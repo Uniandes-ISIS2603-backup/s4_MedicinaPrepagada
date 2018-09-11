d6e292d91b503f60d0b2346d851fde8c12c6fb27
estudiante <estudiante@ISIS2603-323.sis.virtual.uniandes.edu.co>
2018-09-10 21:06:42 -0500

    Modificaciones para bajar duplicados en PacienteLogic

--- s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/ejb/PacienteLogic.java
+++ s4_MedicinaPrepagada-back/src/main/java/co/edu/uniandes/csw/medicinaPrepagada/ejb/PacienteLogic.java
@@ -50,45 +50,14 @@ public class PacienteLogic {
         } catch (ParseException ex) {
             throw new BusinessLogicException("La fecha de nacimiento no cumple el formato");
         }
-        if(persistence.getPacienteByLogin(pacienteEntity.getLogin()) != null){
-            throw new BusinessLogicException("El login dado ya existe");
-        }
-        if(pacienteEntity.getLogin() == null || pacienteEntity.getLogin().equals("")){
-            throw new BusinessLogicException("El login no puede ser vacio o nulo");
-        }
-        String nameValidationPattern = "([A-Z][a-z]+ ){2,3}([A-Z][a-z]+)";
-        Pattern patternName = Pattern.compile(nameValidationPattern);
-        Matcher matchName = patternName.matcher(pacienteEntity.getNombre());
-        if(!matchName.matches()){
-            throw new BusinessLogicException("El formato del nombre no es valido: Nombre Apellidos");
-        }
-        String mailValidationPattern = "[a-z]+@[a-z]+.[a-z]+";
-        Pattern patternMail = Pattern.compile(mailValidationPattern);
-        Matcher matchMail = patternMail.matcher(pacienteEntity.getMail());
-        if(!matchMail.matches()){
-            throw new BusinessLogicException("El mail no sigue el formato de un mail");
-        }
-        // falta revidar casa o apto, solo recibe la direccion sin la parte especifica
-        String direccionValidationPattern = "[A-Z][a-z]{1,2}([A-Z][a-z]+|[0,9]{1,3}[ABC]*#[0-9]{1,3}[ABC]*-[0-9]{2}";
-        Pattern patternDirecccion = Pattern.compile(direccionValidationPattern);
-        Matcher matchDireccion = patternDirecccion.matcher(pacienteEntity.getDireccion());
-        if(!matchDireccion.matches()){
-            throw new BusinessLogicException("La direccion dada no sigue el formato");
-        }
-        String numeroContactoString = pacienteEntity.getNumeroContacto().toString();
-        String numeroContactoValidationPattern = "([0-9]{10}|[0-9]{6})";
-        Pattern patternNumeroContacto = Pattern.compile(numeroContactoValidationPattern);
-        Matcher matchNumeroContacto = patternNumeroContacto.matcher(numeroContactoString);
-        if(!matchNumeroContacto.matches()){
-            throw new BusinessLogicException("El numero de contacto no es valido");
-        }
-        if(pacienteEntity.getDocumentoIdentidad() == 0 || pacienteEntity.getDocumentoIdentidad() == null){
-            throw new BusinessLogicException("el documento de identidad ingresado no es valido");
-        }
         
         //falta revisar que la eps exista
-        
-        return persistence.create(pacienteEntity);
+        if(validarReglasComunes(pacienteEntity)){
+            return persistence.create(pacienteEntity);
+        }
+        else{
+            throw new BusinessLogicException("No se pudo validar una regla de negocio");
+        }
         
     }
     
@@ -120,6 +89,41 @@ public class PacienteLogic {
         if(!pacienteEntity.getFechaNacimiento().equals(oldEntity.getFechaNacimiento())){
             throw new BusinessLogicException("No se puede actualizar la fecha de nacimiento");
         }
+        
+        
+        //falta revisar que la eps exista 
+        
+        List<CitaMedicaEntity> oldCitasMedicas = oldEntity.getCitasMedicas();
+        List<CitaMedicaEntity> newCitasMedicas = pacienteEntity.getCitasMedicas();
+        Iterator ite = oldCitasMedicas.iterator();
+        while(ite.hasNext()){
+            CitaMedicaEntity ent = (CitaMedicaEntity) ite.next();
+            Iterator ite2 = newCitasMedicas.iterator();
+            while(ite2.hasNext()){
+                CitaMedicaEntity ent2 =(CitaMedicaEntity) ite2.next();
+                if(ent.getFecha().compareTo(ent2.getFecha()) == 0 && ent2.getId() == ent.getId()){
+                  throw  new BusinessLogicException("No se puede tener dos citas medicas al tiempo");
+                }
+            }
+        }
+        if(validarReglasComunes(pacienteEntity)){
+            return persistence.update(pacienteEntity);
+        }
+        else{
+            throw new BusinessLogicException("No se cumplio alguna regla de negocio");
+        }
+        
+    }
+    
+    /**
+     * elimina un paciente con el id dado por parametro
+     * @param id: el id del paciente que se desea eliminar
+     */
+    public void deletePaciente(Long id){
+        persistence.delete(id);
+    }
+    
+    public boolean validarReglasComunes(PacienteEntity pacienteEntity)throws BusinessLogicException{
         if(persistence.getPacienteByLogin(pacienteEntity.getLogin()) != null){
             throw new BusinessLogicException("El login dado ya existe");
         }
@@ -155,32 +159,8 @@ public class PacienteLogic {
         if(pacienteEntity.getDocumentoIdentidad() == 0 || pacienteEntity.getDocumentoIdentidad() == null){
             throw new BusinessLogicException("el documento de identidad ingresado no es valido");
         }
-        
-        //falta revisar que la eps exista 
-        
-        List<CitaMedicaEntity> oldCitasMedicas = oldEntity.getCitasMedicas();
-        List<CitaMedicaEntity> newCitasMedicas = pacienteEntity.getCitasMedicas();
-        Iterator ite = oldCitasMedicas.iterator();
-        while(ite.hasNext()){
-            CitaMedicaEntity ent = (CitaMedicaEntity) ite.next();
-            Iterator ite2 = newCitasMedicas.iterator();
-            while(ite2.hasNext()){
-                CitaMedicaEntity ent2 =(CitaMedicaEntity) ite2.next();
-                if(ent.getFecha().compareTo(ent2.getFecha()) == 0 && ent2.getId() == ent.getId()){
-                  throw  new BusinessLogicException("No se puede tener dos citas medicas al tiempo");
-                }
-            }
-        }
-        
-        return persistence.update(pacienteEntity);
-    }
-    
-    /**
-     * elimina un paciente con el id dado por parametro
-     * @param id: el id del paciente que se desea eliminar
-     */
-    public void deletePaciente(Long id){
-        persistence.delete(id);
+        return true;
+         
     }
     
     
