50782b58d7dd57bdf69ae8989ed0a6277efcc855
estudiante <estudiante@ISIS2603-319.sis.virtual.uniandes.edu.co>
2018-09-19 11:58:40 -0500

    Agrega HorarioSedeDTO y especialidades al sql

--- s4_MedicinaPrepagada-api/sql/medisistemas.sql
+++ s4_MedicinaPrepagada-api/sql/medisistemas.sql
@@ -1,9 +1,14 @@
-delete from SedeEntity;
 delete from ConsultorioEntity;
+delete from SedeEntity;
+
 delete from HorarioAtencionEntity;
+delete from MedicoEntity;
+delete from EspecialidadEntity;
 
 
+insert into EspecialidadEntity (nombre) values ('Endocrinologia');
 
+insert into EspecialidadEntity (nombre) values ('Cardiologia');
 
 insert into SedeEntity (id, nombre, direccion, tipoSede, descripcion, latitud, longitud, telefono, correo) 
 values (100, 'super sede 1', 'Calle 153 # 15 - 13', 2, 'Super buena sede', 2, -69.2, 98999423, 'pepito12@hotmail.com' );
@@ -14,25 +19,75 @@ values (200, 'super sede 2', 'Calle 133 # 90 - 01', 1, 'Super buena sede mega', 
 insert into SedeEntity (id, nombre, direccion, tipoSede, descripcion, latitud, longitud, telefono, correo) 
 values (300, 'super sede 3', 'Calle 53 # 14 - 12', 2, 'Super buena sede superx2', 2.1, -69.4, 98065423, 'pepitoSuperman@gmail.com' );
 
-insert into ConsultorioEntity (id, edificio, noficina, sede_id)
-values (100, 'lleras' , 2, 100);
+insert into ConsultorioEntity (id, edificio, noficina, sede_id, especialidad_nombre)
+values (100, 'lleras' , 2, 100, 'Cardiologia');
 
-insert into ConsultorioEntity (id, edificio, noficina, sede_id)
-values (200, 'Camargo' , 3, 100);
+insert into ConsultorioEntity (id, edificio, noficina, sede_id , especialidad_nombre)
+values (200, 'Camargo' , 3, 100, 'Cardiologia');
 
-insert into ConsultorioEntity (id, edificio, noficina, sede_id)
-values (300, 'Alberto' , 1, 200);
+insert into ConsultorioEntity (id, edificio, noficina, sede_id , especialidad_nombre)
+values (300, 'Alberto' , 1, 200, 'Cardiologia');
 
-insert into ConsultorioEntity (id, edificio, noficina, sede_id)
-values (400, 'Julio' , 5, 200);
+insert into ConsultorioEntity (id, edificio, noficina, sede_id , especialidad_nombre)
+values (400, 'Julio' , 5, 200, 'Endocrinologia');
 
-insert into ConsultorioEntity (id, edificio, noficina, sede_id)
-values (500, 'Principal' , 1, 300);
+insert into ConsultorioEntity (id, edificio, noficina, sede_id , especialidad_nombre)
+values (500, 'Principal' , 1, 300, 'Endocrinologia');
 
-insert into ConsultorioEntity (id, edificio, noficina, sede_id)
-values (600, 'Samuel' , 7, 300);
+insert into ConsultorioEntity (id, edificio, noficina, sede_id , especialidad_nombre)
+values (600, 'Samuel' , 7, 300, 'Endocrinologia');
 
-insert into ConsultorioEntity (id, edificio, noficina, sede_id)
-values (700, 'Adios' , 20, 300);
+insert into ConsultorioEntity (id, edificio, noficina, sede_id , especialidad_nombre)
+values (700, 'Adios' , 20, 300, 'Endocrinologia');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
